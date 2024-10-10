ad33750e2116b866344c3b367d116485f2bdd9d1
vershinsky <yvvershinsky@rosatom.ru>
2024-10-10 13:13:39 +0700

    Implement `MovieEntity` Data Class

---
+++ app/src/main/java/com/cyberbyte/mymovielibrary/MovieEntity.kt
@@ -0,0 +1,13 @@
+package com.cyberbyte.mymovielibrary
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "movies")
+data class MovieEntity(
+    @PrimaryKey val id: Int,
+    val title: String,
+    val description: String,
+    val releaseDate: String,
+    val rating: Float
+)
